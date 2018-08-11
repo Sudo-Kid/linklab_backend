c1f9987ff675055c8eeca8e42e23c65b3208dbe6
The-Kid89 <speer.emett@gmail.com>
2018-08-11 12:05:30 -0700

    Updated API for Staghouse

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from functools import reduce
 
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from . import models
@@ -24,9 +25,31 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserProfile(serializers.ModelSerializer):
-    social_display_settings = SocialDisplaySettings(many=True)
+    services = serializers.SerializerMethodField()
+    services_order = serializers.SerializerMethodField()
     user = UserSerializer(many=False)
 
     class Meta:
         model = models.UserProfile
-        fields = ('user', 'template', 'social_display_settings')
+        fields = (
+            'user',
+            'template',
+            'services',
+            'services_order'
+        )
+
+    @staticmethod
+    def get_services(obj):
+        social_settings = {}
+        for site in obj.services.all():
+            social_settings[site.name] = {
+                "limit": site.limit,
+            }
+        return social_settings
+
+    @staticmethod
+    def get_services_order(obj):
+        social_settings = []
+        for site in obj.services.all():
+            social_settings.append(site.name)
+        return social_settings
