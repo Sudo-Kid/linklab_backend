15ee8b6695e418b85f6f7e0dabc70a21f60ed0e5
The-Kid89 <speer.emett@gmail.com>
2018-08-12 02:00:28 -0700

    Fixed static files

--- linklab/settings.py
+++ linklab/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = BASE_DIR + STATIC_URL
 
 ACCOUNT_EMAIL_REQUIRED = False
 SOCIALACCOUNT_PROVIDERS = {
